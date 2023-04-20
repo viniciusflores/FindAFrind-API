8c0122cdd593c788062a94aa97d6eb846f0d295e
Vinicius Flores <viniciusflores379@gmail.com>
2023-04-20 18:12:47 -0300

    :sparkles: Implement global error handle

--- src/http/controllers/orgs/create-org.ts
+++ src/http/controllers/orgs/create-org.ts
@@ -33,7 +33,7 @@ export async function createOrg(request: FastifyRequest, reply: FastifyReply) {
     if (err instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: err.message })
     }
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
