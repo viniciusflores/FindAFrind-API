e07d3badc7170f36b6a53baaac0f68ae27dbc4de
Vinicius Flores <viniciusflores379@gmail.com>
2023-04-20 16:27:55 -0300

    :boom: New database model

---
+++ prisma/migrations/20230420192534_create_org_and_pet/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "PetAgeDescription" AS ENUM ('FILHOTE', 'JOVEM', 'ADULTO', 'IDOSO');
+
+-- CreateEnum
+CREATE TYPE "PetSizeDescription" AS ENUM ('PEQUENO', 'MEDIO', 'GRANDE');
+
+-- CreateEnum
+CREATE TYPE "PetLevelEnergy" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "PetGender" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateEnum
+CREATE TYPE "PetLevelIndependence" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "org" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "adress" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "org_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pet" (
+    "id" TEXT NOT NULL,
+    "org_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "ageDescription" "PetAgeDescription" NOT NULL,
+    "size" "PetSizeDescription" NOT NULL,
+    "energy" "PetLevelEnergy" NOT NULL,
+    "independence" "PetLevelIndependence" NOT NULL,
+
+    CONSTRAINT "pet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "org_email_key" ON "org"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "org_phone_key" ON "org"("phone");
+
+-- AddForeignKey
+ALTER TABLE "pet" ADD CONSTRAINT "pet_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "org"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
